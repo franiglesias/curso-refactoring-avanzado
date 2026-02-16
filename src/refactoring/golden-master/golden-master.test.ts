37190b8679106098b3b228e9a809e26e76f5b902
Fran Iglesias <franiglesias@mac.com>
2026-02-16 17:32:07 +0100

    Golde-master and seams example

--- src/refactoring/golden-master/golden-master.test.ts
+++ src/refactoring/golden-master/golden-master.test.ts
@@ -3,9 +3,12 @@ import {generateOrder, ReceiptPrinter} from './golden-master'
 
 describe('Receipt Printer', () => {
   let counter = 0
-  describe.each(['Ana', 'Luis', 'Mar', 'Iván', 'Sofía'])('Given a customer %s', (customer) => {
-    describe.each([1, 2, 3, 4, 5])('Given a number of items %d', (item) => {
-      describe.each([1, 3, 10, 25, 300])('Given quantity %d', (quantity) => {
+  describe('Given a customer', () => {
+    const customer = 'Ana'
+    describe('Given a number of items', () => {
+      const item = 1
+      describe('Given quantity', () => {
+        const quantity = 1
         it('should print a receipt', () => {
           counter = counter + 1
           const pedido = generateOrder('ORD-' + counter.toString(), customer, item, quantity)
@@ -13,32 +16,5 @@ describe('Receipt Printer', () => {
         })
       })
     })
-  })
-
-  it('should generate a golden master', () => {
-    const printer = new ReceiptPrinter()
-
-    // Combinations of
-
-    const customers = ['Ana', 'Luis', 'Mar', 'Iván', 'Sofía']
-    const items = [1, 2, 3, 4, 5]
-    const quantities = [1, 3, 10, 25, 300]
-
-    let receipts = []
-    let counter = 0
-    for (const customer of customers) {
-      for (const i of items) {
-        for (const quantity of quantities) {
-          counter = counter + 1
-          const pedido = generateOrder('ORD-' + counter.toString(), customer, i, quantity)
-          const receipt = printer.print(pedido)
-          receipts.push(receipt)
-        }
-      }
-    }
-
-    console.log(`Generated ${receipts.length} receipts`)
-
-    expect(receipts.join(`\n==================\n`)).matchSnapshot()
   })
 })
